073b26a9c9008ce1551593362de17406233289c1
snaker018g <snaker018@126.com>
2022-06-01 23:01:19 +0800

    PermissionRequest Update

--- PermissionRequest/Application/src/main/java/uk/co/webwrights/permissions/PermissionsFragment.java
+++ PermissionRequest/Application/src/main/java/uk/co/webwrights/permissions/PermissionsFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.view.View;
 
 
 public class PermissionsFragment extends Fragment {
